2e8a3e4740992cde9782f5c7f2f7da52ea4e04c8
@sgtn <shogo1104@gmail.com>
2015-02-15 21:30:16 +0900

    refactoring

--- slidemilk.js
+++ slidemilk.js
@@ -1,27 +1,18 @@
 var webdriver = require('selenium-webdriver'),
+    argv = require('argv'),
+    MilkCocoa = require('./milkcocoa-node'),
     By = webdriver.By,
-    fs = require("fs");
-    Promise = webdriver.promise.Promise,
-    argv = require('argv');
-
-var MilkCocoa = require('./milkcocoa-node'),
     milkcocoa = new MilkCocoa("https://io-ai02l9tke.mlkcca.com"),
-    rightBtnServer = milkcocoa.dataStore("rbtn"),
-    leftBtnServer = milkcocoa.dataStore("lbtn");
-
-var targets = argv.run().targets;
-
-var chromeDriver = new webdriver.Builder()
-                        .usingServer('http://localhost:4444/wd/hub')
+    chromeDriver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub')
                         .withCapabilities( webdriver.Capabilities.chrome() )
                         .build();
 
-chromeDriver.get(targets[0]);
+chromeDriver.get(argv.run().targets[0]);
 
-rightBtnServer.on("send", function(e){
+milkcocoa.dataStore("rightButtonServer").on("send", function(e){
     ( new webdriver.ActionSequence(chromeDriver) ).sendKeys(webdriver.Key.RIGHT).perform();
 });
 
-leftBtnServer.on("send", function(e){
+milkcocoa.dataStore("leftButtonServer").on("send", function(e){
     ( new webdriver.ActionSequence(chromeDriver) ).sendKeys(webdriver.Key.LEFT).perform();
 });
